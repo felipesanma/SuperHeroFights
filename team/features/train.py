bd573a12ea7cd537b8f696e772fd0e58c1b6d78f
felipesanma <felipesmsoto>
2022-08-04 18:16:19 -0400

    train methods added

--- team/features/train.py
+++ team/features/train.py
@@ -8,3 +8,39 @@ class TeamTraining:
 
     def __init__(self, *, members: list):
         self.members = members
+
+    def actual_stamina(self):
+        """
+        AS: Actual Stamina, valor aleatorio antre 0 y 10
+        """
+        NotImplementedError
+
+    def hp(self):
+        NotImplementedError
+
+    def get_team_alignment(self):
+        """
+        Alignment: será el de la mayoría.
+        """
+        NotImplementedError
+
+    def fb(self):
+        """
+        Filiation Coefficient: bonus o penalización, según la naturaleza del personaje vs la de su equipo
+        """
+        NotImplementedError
+
+    def fight_stats(self):
+        NotImplementedError
+
+    def mental_attack(self):
+        NotImplementedError
+
+    def strong_attack(self):
+        NotImplementedError
+
+    def fast_attack(self):
+        NotImplementedError
+
+    def all_attacks(self):
+        NotImplementedError
--- team/team.py
+++ team/team.py
@@ -3,7 +3,7 @@ from .features import MAX_HERO_MEMBERS, TeamManagement, TeamTraining
 
 class Team:
     """
-    Generador de equipo (máximo 5 miembros)
+    Generador de equipo (máximo {MAX_HERO_MEMBERS} miembros)
     """
 
     def __init__(self, *, members: list = [], ready_to_fight: bool = False):
@@ -13,13 +13,19 @@ class Team:
         self.members = self.manage.members
         self.members_by_id = self.manage.members_id
         self.ready_to_fight = ready_to_fight
-        # self.alignment = alignment
-        # self.ready_to_fight = ready_to_fight
+        self.alignment = None
+        self.ready_to_fight = ready_to_fight
 
-    def set_train(self):
+    def _set_train(self):
         if len(self.manage.members) != MAX_HERO_MEMBERS:
             n_of_heroes = MAX_HERO_MEMBERS - len(self.manage.members)
             self.manage.add_multiple_random(n_of_heroes)
         self.train = TeamTraining(members=self.manage.members)
         print("Team ready to train")
         print(self.manage.members_id)
+
+    def create(self):
+        self._set_train()
+
+    def prepare_to_fight(self):
+        NotImplementedError
