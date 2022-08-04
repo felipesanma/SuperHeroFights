c37c1e08955435033dd6bd412540212bbd9585eb
felipesanma <felipesmsoto>
2022-08-04 03:56:01 -0400

    error if not enough members

--- team/features/train.py
+++ team/features/train.py
@@ -7,8 +7,7 @@ class TeamTraining:
     """
 
     def __init__(self, *, members: list):
-        print("len(members)", len(members))
-        print("MAX_HERO_MEMBERS: ", MAX_HERO_MEMBERS)
+
         if len(members) != MAX_HERO_MEMBERS:
             raise ("Not enough members. For train a team you need more members")
         self.members = members
