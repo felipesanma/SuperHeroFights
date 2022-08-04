672a3cfd4e9600840dcff16bfffb1baa4adaf2fd
felipesanma <felipesmsoto>
2022-08-04 02:44:30 -0400

    multiple add method added

--- team/features/manage.py
+++ team/features/manage.py
@@ -9,7 +9,7 @@ class TeamManagement:
     Administración del equipo
     """
 
-    def __init__(self, *, members: list):
+    def __init__(self, *, members: list) -> None:
         self.members = members
         self.members_names = []
         self.members_id = {}
@@ -17,12 +17,12 @@ class TeamManagement:
     def add_member_by_id(self, id: int):
 
         if id not in POSSIBLE_HEROE_ID:
-            msg = f"No heroe found for id={id}, try with another id"
+            msg = f"Sorry! No heroe found for id={id}, try with another id"
             print(msg)
             return msg, 404
 
         if len(self.members_names) == MAX_HERO_MEMBERS:
-            msg = f"Team already has 5 members, try removing members first"
+            msg = f"Team already has {MAX_HERO_MEMBERS} members, try removing members first. Hero id: {id}"
             print(msg)
             return msg, 405
 
@@ -45,16 +45,16 @@ class TeamManagement:
     def add_random_member(self):
 
         if len(self.members_names) == MAX_HERO_MEMBERS:
-            msg = f"Team already has 5 members, try removing members first"
+            msg = f"Team already has {MAX_HERO_MEMBERS} members, try removing members first. Hero id: {id}"
             print(msg)
             return msg, 405
 
         id = random.choice(tuple(POSSIBLE_HEROE_ID))
 
-        if id in self.members_id:
-            msg = f"Heroe id={id}, already exist in team. Try with another id"
+        while id in self.members_id:
+            msg = f"Heroe id={id}, already exist in team. Trying with another id"
             print(msg)
-            return msg, 406
+            id = random.choice(tuple(POSSIBLE_HEROE_ID))
 
         superhero = SuperHero(id=id)
         superheroe, status_code = superhero.characters.get_complete_information()
@@ -66,3 +66,11 @@ class TeamManagement:
         msg = f"NEW HEROE ADDED: {superheroe.name}"
         print(msg)
         return msg, status_code
+
+    def add_multiple_by_id(self, ids: list) -> None:
+        for id in ids:
+            self.add_member_by_id(id)
+
+    def add_multiple_random(self, quantity: int) -> None:
+        for i in range(quantity):
+            self.add_random_member()
--- team_test.py
+++ team_test.py
@@ -1,9 +1,8 @@
 from team import Team
 
-team = Team()
-
-
+"""
 # Test add_member_by_id method
+team = Team()
 hero_id = 1
 new_member = team.manage.add_member_by_id(hero_id)
 print(team.members_by_id)
@@ -21,3 +20,10 @@ print(team.members_by_id)
 new_member = team.manage.add_random_member()
 new_member = team.manage.add_random_member()
 print(team.members_by_id)
+
+"""
+# Test multiple add by id
+a_lot_of_heroes = [1, 30, 25, 38, 200, 400, 500]
+team_2 = Team()
+team_2.manage.add_multiple_by_id(a_lot_of_heroes)
+print(team_2.members_by_id)
