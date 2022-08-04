460afefdd3639e92d64836ece6a8f80784cdb327
felipesanma <felipesmsoto>
2022-08-04 01:16:39 -0400

    add random member features

--- team/features/manage.py
+++ team/features/manage.py
@@ -1,4 +1,12 @@
+import random
+
 from .superhero import SuperHero
+
+# Reference: https://akabab.github.io/superhero-api/api/glossary.html
+FIRST_HERO_ID = 1
+LAST_HERO_ID = 731
+# Declared on task
+MAX_HERO_MEMBERS = 5
 
 
 class TeamManagement:
@@ -12,6 +20,41 @@ class TeamManagement:
         self.members_id = {}
 
     def add_member_by_id(self, id: int):
+
+        if len(self.members_names) == MAX_HERO_MEMBERS:
+            msg = f"Team already has 5 members, try removing members first"
+            print(msg)
+            return msg, 405
+
+        superhero = SuperHero(id=id)
+        superheroe, status_code = superhero.characters.get_complete_information()
+
+        if status_code != 200:
+            msg = f"No heroe found for id={id}, try with another id"
+            print(msg)
+            return msg, status_code
+
+        if superheroe.id in self.members_id:
+            msg = f"Heroe id={id}, already exist in team. Try with another id"
+            print(msg)
+            return msg, 406
+
+        self.members.append(superheroe)
+        self.members_names.append(superheroe.name)
+        self.members_id[superheroe.id] = superheroe.name
+
+        msg = f"NEW HEROE ADDED: {superheroe.name}"
+        print(msg)
+        return msg, 200
+
+    def add_random_member(self):
+
+        if len(self.members_names) == MAX_HERO_MEMBERS:
+            msg = f"Team already has 5 members, try removing members first"
+            print(msg)
+            return msg, 405
+
+        id = random.randint(FIRST_HERO_ID, LAST_HERO_ID)
 
         superhero = SuperHero(id=id)
         superheroe, status_code = superhero.characters.get_complete_information()
--- team_test.py
+++ team_test.py
@@ -4,5 +4,5 @@ team = Team()
 hero_id = 1
 no_id = 0
 new_member = team.manage.add_member_by_id(hero_id)
-new_member = team.manage.add_member_by_id(no_id)
+new_member = team.manage.add_random_member(no_id)
 print(team.members)
